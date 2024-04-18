a6f0135d669a711984207cb85c89339de602e39f
Kublo <jakubwozniak0611@gmail.com>
2024-04-18 17:08:09 +0200

    initial commit for app using sdk

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,39 @@
 'use client'
 
-import React from 'react';
-import {WorkspacesProvider} from "@/app/components/Reducers/WorkspacesReducer";
-import {App} from "@/app/components/App";
+import React, {useState} from 'react';
+import {Workspace} from 'seco-sdk-react';
 
 const WorkspacePage = () => {
+    const handleEvent = (event: any) => {
+        console.log(event)
+    }
+
+    const [subscriptions, setSubscriptions] = useState<{ [event: string]: ((e: any) => void) }>({
+        "afterScenarioExecutionExt": handleEvent,
+        "afterScenarioExecution": handleEvent,
+    })
+
+    const workspaceId = "b24c9cebeefe4f5180f31aa80c6139ee"
+    const width = "100%"
+    const height = "100%"
+
+    const changeSubscription = () => {
+        if(subscriptions["afterScenarioExecution"]) {
+            setSubscriptions({
+                "afterScenarioExecutionExt": handleEvent,
+            })} else {
+            setSubscriptions({
+                "afterScenarioExecution": handleEvent,
+                "afterScenarioExecutionExt": handleEvent,
+            })
+        }
+    }
+
     return (
-        <div className="flex row w-full">
-            <WorkspacesProvider>
-                <App/>
-            </WorkspacesProvider>
+        <div className="w-full h-[100vh]">
+            <Workspace workspaceId={workspaceId} width={width} height={height} visible={true}
+                       subscriptions={subscriptions}/>
+            <button onClick={changeSubscription}>Change subscription</button>
         </div>
     );
 }
