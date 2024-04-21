49641fcaddc2fbd550c91d9d2659c81899329755
Kublo <jakubwozniak0611@gmail.com>
2024-04-21 22:56:17 +0200

    small code fix and added needed dependencies

--- app/components/Panels/VisibilityPanel.tsx
+++ app/components/Panels/VisibilityPanel.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode, useContext} from "react";
+import React, {useContext} from "react";
 import {WorkspaceContext, WorkspaceDispatchContext} from "@/app/components/Reducers/WorkspaceReducer";
 
 const SIZES = {
